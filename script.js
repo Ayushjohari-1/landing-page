01dce7a8d450ec20f95e97266596e48c86a4b069
Ayush Johari <ayushjohari49@gmail.com>
2024-09-23 12:43:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Loader-section Started Here..//
+
+function loaderAnimation(){
+
+   var loader = document.querySelector(".loader")
+
+   setTimeout(function(){
+      loader.style.top = "100%"
+   },4200)
+}
+
+loaderAnimation();
+
+// Loader-section End Here..//
+
+
+// let hamburger = document.querySelector(".hamburger");
+// let ul = document.querySelector("ul");
+
+// hamburger.addEventListener("click", () => {
+//     ul.classList.toggle("ham_active");
+   
+// });
+
+
+
